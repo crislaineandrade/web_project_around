b3357fcb068e790920398e6a0c62951be26dc595
Crislaine Andrade <c.mrochaandrade@gmail.com>
2024-11-01 16:18:09 -0400

    feature:popup open edit images

--- script.js
+++ script.js
@@ -1,24 +1,80 @@
-const openButton = document.querySelector('.profile__image-edit-button'); //botao de editar
-const closeButton = document.querySelector('.popup__close-button'); //botao x de fechar
-const formElement = document.querySelector('.popup'); //formulario (popup)
+//botao de editar
+const openButton = document.querySelector('.profile__image-edit-button');
+//botao x de fechar
+const closeButton = document.querySelector('.popup__close-button');
+//formulario (popup)
+const formElement = document.querySelector('.popup');
 //pegar o que esta escrito no h1 (nome)
 const nameUser = document.querySelector('.profile__user') // nome do usuario
 //pegar o que esta escrito no paragrafo (ocupacao)
-const ocupationUser = document.querySelector('.profile__paragraph') //ocupacao do usuario
-const saveButton = document.querySelector('.popup__save-button') //botao de salvar
-const inputName = document.querySelector('.popup__name') //input do nome
-const inputOcupation = document.querySelector('.popup__ocupation') //input da ocupacao
+const ocupationUser = document.querySelector('.profile__paragraph');
+//botao de salvar
+const saveButton = document.querySelector('.popup__save-button');
+//input do nome
+const inputName = document.querySelector('.popup__name');
+//input da ocupacao
+const inputOcupation = document.querySelector('.popup__ocupation');
 
-function openForm() {
-  formElement.classList.add('popup_opened')
+//botao de editar
+// const popupEditForm = document.querySelector('.profile__add-button');
+
+//formulario de adcionar imagem (popup add)
+const formAddImage = document.querySelector('.popup-edit')
+//botao de adicionar imagem
+const addImageButton = document.querySelector('.profile__add-button')
+//botao de fechar popup adcionar imagem
+const closeEditButton = document.querySelector('.popup-edit__close-button')
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+function openForm(formReceived) {
+  formReceived.classList.add('popup_opened')
 }
-openButton.addEventListener('click', openForm);
+openButton.addEventListener('click', function() {
+  openForm(formElement);
+})
+addImageButton.addEventListener('click',function() {
+  openForm(formAddImage);
+});
 
-function closeForm() {
-  formElement.classList.remove('popup_opened');
+
+function closeForm(formReceived) {
+  formReceived.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', closeForm);
+closeButton.addEventListener('click', function(){
+  closeForm(formElement);
+});
+closeEditButton.addEventListener('click', function (){
+  closeForm(formAddImage);
+})
+
+
 
 
 function handleProfileFormSubmit(evt) {
